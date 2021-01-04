790249c7e395191024e3baa849c2ec6a45d18eff
DamirStipa <76243655+DamirStipa@users.noreply.github.com>
2021-01-04 17:32:25 +0100

    2 version of RFC

---
+++ random_forest_classification.py
@@ -0,0 +1,213 @@
+import numpy as np
+import mahotas
+from cv2 import cv2
+import os
+import h5py
+import glob
+import pickle
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import KFold, StratifiedKFold
+from skimage.feature import hog
+import time
+
+tic = time.perf_counter()
+def unpickle(file):
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
+
+def unpickle_labels(file):
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo)
+    return dict
+
+cifar_data1 = unpickle("C:\\Users\\stipa\\Desktop\\random_forest\\dataset\\train\\data_batch_1")
+cifar_data2 = unpickle("C:\\Users\\stipa\\Desktop\\random_forest\\dataset\\train\\data_batch_2")
+cifar_data3 = unpickle("C:\\Users\\stipa\\Desktop\\random_forest\\dataset\\train\\data_batch_3")
+cifar_data4 = unpickle("C:\\Users\\stipa\\Desktop\\random_forest\\dataset\\train\\data_batch_4")
+cifar_data5 = unpickle("C:\\Users\\stipa\\Desktop\\random_forest\\dataset\\train\\data_batch_5")
+unpickled_lables = unpickle_labels("C:\\Users\\stipa\\Desktop\\random_forest\\dataset\\train\\batches.meta")
+test_data = unpickle("C:\\Users\\stipa\\Desktop\\random_forest\\dataset\\test\\test_batch")
+
+train_labels = unpickled_lables['label_names']
+
+array = []
+array1 = cifar_data1[b'data']
+array2 = cifar_data2[b'data']
+array3 = cifar_data3[b'data']
+array4 = cifar_data4[b'data']
+array5 = cifar_data5[b'data']
+for item in array1:
+    array.append(item)
+for item in array2:
+    array.append(item)
+for item in array3:
+    array.append(item)
+for item in array4:
+    array.append(item)
+for item in array5:
+    array.append(item)
+array = np.reshape(array, (50000, 3, 32, 32))
+
+test_array = test_data[b'data']
+test_array = np.reshape(test_array, (10000, 3, 32, 32))
+
+images = []
+test_images = []
+for item in array:
+    images.append(np.transpose(item, (1,2,0)))
+
+for item in images:
+    item = cv2.cvtColor(item, cv2.COLOR_RGB2BGR)
+
+for item in test_array:
+    test_images.append(np.transpose(item, (1,2,0)))
+
+for item in test_images:
+    item = cv2.cvtColor(item, cv2.COLOR_RGB2BGR)
+
+cifar_labels = []
+cifar_labels1 = cifar_data1[b'labels']
+cifar_labels2 = cifar_data2[b'labels']
+cifar_labels3 = cifar_data3[b'labels']
+cifar_labels4 = cifar_data4[b'labels']
+cifar_labels5 = cifar_data5[b'labels']
+
+for item in cifar_labels1:
+    cifar_labels.append(item)
+for item in cifar_labels2:
+    cifar_labels.append(item)
+for item in cifar_labels3:
+    cifar_labels.append(item)
+for item in cifar_labels4:
+    cifar_labels.append(item)
+for item in cifar_labels5:
+    cifar_labels.append(item)
+
+fixed_size = tuple((200,200))
+
+h5_data = 'C:\\Users\\stipa\\Desktop\\random_forest\\output\\data.h5'
+h5_labels = 'C:\\Users\\stipa\\Desktop\\random_forest\\output\\labels.h5'
+bins = 8
+scoring    = "accuracy"
+
+#Feature descriptor: Hu Moments
+def fd_hu_moments(image):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    feature = cv2.HuMoments(cv2.moments(image)).flatten()
+    return feature
+
+#Feature descriptor: Haralick Texture
+def fd_haralick_texture(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    haralick = mahotas.features.haralick(gray).mean(axis=0)
+    return haralick
+
+#Feature descriptor: Color Histogram
+def fd_color_histogram(image, mask = None):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    hist = cv2.calcHist([image], [0,1,2], None, [bins, bins, bins], [0, 256, 0, 256, 0, 256])
+    cv2.normalize(hist, hist)
+    return hist.flatten()
+
+#Feature descriptor: Histogram of Gradients
+def fd_hog(image):
+    return hog(image, orientations=9, pixels_per_cell=(8,8), cells_per_block=(2,2), multichannel=True)
+    
+
+global_features = []
+labels = []
+
+for i in range(1000):
+    curr_image = cv2.resize(images[i], fixed_size)
+
+    fv_hu_moments = fd_hu_moments(curr_image)
+    fv_haralick_texture = fd_haralick_texture(curr_image)
+    fv_color_histogram = fd_color_histogram(curr_image)
+    fv_hog = fd_hog(curr_image)
+
+    global_feature = np.hstack([fv_color_histogram, fv_haralick_texture, fv_hu_moments, fv_hog])
+
+    labels.append(train_labels[cifar_labels[i]])
+    global_features.append(global_feature)
+    print(i)
+    
+# get the overall feature vector size
+#print("[STATUS] feature vector size {}".format(np.array(global_features).shape))
+
+# get the overall training label size
+#print("[STATUS] training Labels {}".format(np.array(labels).shape))
+
+# encode the target labels
+targetNames = np.unique(labels)
+le          = LabelEncoder()
+target      = le.fit_transform(labels)
+
+#Now that we have the features we can scale them
+scaler = MinMaxScaler(feature_range=(0, 1))
+rescaled_features = scaler.fit_transform(global_features)
+
+#print("[STATUS] target labels: {}".format(target))
+#print("[STATUS] target labels shape: {}".format(target.shape))
+
+num_trees = 1000
+test_size = 0.10
+seed      = 9
+
+#(trainDataGlobal, testDataGlobal, trainLabelsGlobal, testLabelsGlobal) = train_test_split(np.array(rescaled_features),
+                                                                                          #np.array(target),
+                                                                                          #test_size=test_size,
+                                                                                          #random_state=seed)
+
+#print(trainLabelsGlobal.shape)
+#print(testLabelsGlobal.shape)
+#print(trainDataGlobal.shape)
+#print(testDataGlobal.shape)
+
+#kfold = KFold(n_splits=10, random_state=seed, shuffle=True)
+#cv_results = cross_val_score(RandomForestClassifier(n_estimators=num_trees, random_state=seed), trainDataGlobal, trainLabelsGlobal, cv=kfold, scoring=scoring)
+
+#plt.boxplot(cv_results)
+#plt.show()
+
+toc = time.perf_counter()
+print(f"Extracted features in {toc - tic:0.4f} seconds")
+print("\n Training RFC...")
+tik = time.perf_counter()
+clf  = RandomForestClassifier(n_estimators=num_trees, random_state=seed)
+
+clf.fit(rescaled_features, target)
+tok = time.perf_counter()
+print(f"Training finished in {tok-tik:0.4f} seconds")
+
+for image in test_images:
+    curr_image = cv2.resize(image, fixed_size)
+
+    fv_hu_moments = fd_hu_moments(curr_image)
+    fv_haralick_texture = fd_haralick_texture(curr_image)
+    fv_color_histogram = fd_color_histogram(curr_image)
+    fv_hog = fd_hog(curr_image)
+
+    global_feature = np.hstack([fv_color_histogram, fv_haralick_texture, fv_hu_moments, fv_hog])
+    #global_feature = global_feature.reshape(-1,2)
+    #print(global_feature)
+
+    # scale features in the range (0-1)
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    rescaled_feature = scaler.fit_transform(global_feature.reshape(-1,1))
+    #print(rescaled_feature)
+
+    # predict label of test image
+    prediction = clf.predict(rescaled_feature.reshape(1,-1))[0]
+    print(prediction)
+
+    # show predicted label on image
+    cv2.putText(curr_image, train_labels[prediction], (20,30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0,255,255), 3)
+
+    # display the output image
+    plt.imshow(cv2.cvtColor(curr_image, cv2.COLOR_BGR2RGB))
+    plt.show()
---
+++ rfc_v2.py
@@ -0,0 +1,119 @@
+from sklearn import metrics
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
+import time
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+from cv2 import cv2
+
+def unpickle(file):
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
+
+def unpickle_labels(file):
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo)
+    return dict
+
+start_time = time.time()
+
+x_train = []
+y_train = []
+x_test = []
+y_test = []
+
+cifar_data1 = unpickle("C:\\Users\\stipa\\Desktop\\random_forest\\dataset\\train\\data_batch_1")
+'''cifar_data2 = unpickle("C:\\Users\\stipa\\Desktop\\random_forest\\dataset\\train\\data_batch_2")
+cifar_data3 = unpickle("C:\\Users\\stipa\\Desktop\\random_forest\\dataset\\train\\data_batch_3")
+cifar_data4 = unpickle("C:\\Users\\stipa\\Desktop\\random_forest\\dataset\\train\\data_batch_4")
+cifar_data5 = unpickle("C:\\Users\\stipa\\Desktop\\random_forest\\dataset\\train\\data_batch_5")'''
+#unpickled_lables = unpickle_labels("C:\\Users\\stipa\\Desktop\\random_forest\\dataset\\train\\batches.meta")
+test_data = unpickle("C:\\Users\\stipa\\Desktop\\random_forest\\dataset\\test\\test_batch")
+
+array1 = cifar_data1[b'data']
+'''array2 = cifar_data2[b'data']
+array3 = cifar_data3[b'data']
+array4 = cifar_data4[b'data']
+array5 = cifar_data5[b'data']'''
+for item in array1:
+    x_train.append(item)
+'''for item in array2:
+    x_train.append(item)
+for item in array3:
+    x_train.append(item)
+for item in array4:
+    x_train.append(item)
+for item in array5:
+    x_train.append(item)'''
+
+X_train = np.array(x_train)
+X_train = X_train.reshape(10000, 3072)
+X_train = X_train.astype("float32")
+
+x_test = test_data[b'data']
+x_test_cv = np.reshape(x_test, (10000, 3, 32, 32))
+test_cv = []
+for item in x_test_cv:
+    test_cv.append(np.transpose(item, (1,2,0)))
+
+X_test = np.array(x_test)
+X_test = X_test.reshape(10000, 3072)
+X_test = X_test.astype("float32")
+
+cifar_labels1 = cifar_data1[b'labels']
+'''cifar_labels2 = cifar_data2[b'labels']
+cifar_labels3 = cifar_data3[b'labels']
+cifar_labels4 = cifar_data4[b'labels']
+cifar_labels5 = cifar_data5[b'labels']'''
+
+for item in cifar_labels1:
+    y_train.append(item)
+'''for item in cifar_labels2:
+    y_train.append(item)
+for item in cifar_labels3:
+    y_train.append(item)
+for item in cifar_labels4:
+    y_train.append(item)
+for item in cifar_labels5:
+    y_train.append(item)'''
+
+Y_train = np.array(y_train)
+
+y_test = test_data[b'labels']
+Y_test = np.array(y_test)
+Y_train = Y_train.flatten()
+Y_test = Y_test.flatten()
+
+print(X_train.shape)
+print(X_test.shape)
+print(Y_train.shape)
+print(Y_test.shape)
+
+X_train /= 255.
+X_test /= 255.
+
+rfc_model = RandomForestClassifier(n_estimators=100)
+
+cv_results = cross_val_score(rfc_model, X_train, Y_train, cv=5, scoring='accuracy')
+
+plt.boxplot(cv_results)
+plt.show()
+
+print("STATUS: Training RFC...")
+rfc_model.fit(X_train, Y_train)
+
+for i in range(len(X_test)):
+    predicted = rfc_model.predict(X_test[i].reshape(1,-1))
+    expected = Y_test[i]
+    print(predicted)
+    # show predicted label on image
+    cv2.putText(cv2.UMat(test_cv[i]), str(predicted), (10,10), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0,255,255), 3)
+
+    # display the output image
+    plt.imshow(cv2.cvtColor(test_cv[i], cv2.COLOR_RGB2BGR))
+    plt.show()
+
+    #0-airplane    1-automobile    2-bird    3-cat    4-deer    5-dog   6-frog    7-horse   8-ship    9-truck
